fce5cf9050f4f17e0907e23888fe8051213f9acc
GeunYoung2 <42885688+GeunYoung2@users.noreply.github.com>
2018-09-02 17:45:10 +0900

    Add files via upload

---
+++ 0_ntering and printing your name.py
@@ -0,0 +1,11 @@
+# Enter your last name:
+# **Grimson
+# Enter your first name:
+# **eric
+# eric
+# Grimson
+
+last = input("Enter your last name:")
+first = input("Enter your first name:")
+print(last)
+print(first)
